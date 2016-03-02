84d709d6c8d6f67c8749edf05e53890c9cfe1e78
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2016-03-02 02:24:39 -0300

    Carregamento de parâmetros modificado

--- src/configure.m
+++ src/configure.m
@@ -1,27 +1,21 @@
 function configure
 %CONFIGURE Lê configurações e muda parâmetros do modelo.
+	global parameters;
 
-	if exist('parameters.mat', 'file') == 2
-		load('parameters.mat');
-	else
-		parameters = struct('k', 1, 'ro', 1, 'cp', 1, 'alpha', [], 'length', 0.01, 'subdiv', 10);
-	end
+	load_parameters(1);
 
-	display_parameters();
 	display('Todos os parâmetros podem ser alterados, menos α (coeficiente de');
-	display('difusão térmica), que será calculado em função de k, ρ e cp');
-
+	display('difusão térmica), que será calculado em função de k, ρ e cp.');
 
 	while ~strcmp(key = input('Digite um parâmetro para modificar (ou Enter para sair): ', 's'), '')
 		value = input(sprintf('Novo valor de %s: ', key));
 		parameters.(key) = value;
 	end
 
-	parameters.alpha = parameters.k / (parameters.ro * parameters.cp);
-	display(sprintf('Desses parâmetros, calcula-se alpha = %e [W * m² / J]', parameters.alpha));
+	display(sprintf('Desses parâmetros, calcula-se alpha = %e [W * m² / J]',
+		parameters.k / (parameters.ro * parameters.cp)));
 
 	save('parameters.mat', 'parameters');
 	display('Configuração escolhida:');
-	display_parameters();
-
+	load_parameters(1);
 end
--- src/load_parameters.m
+++ src/load_parameters.m
@@ -1,9 +1,18 @@
-function display_parameters
-%DISPLAY_PARAMETERS Função que mostra na tela parâmetros atuais do modelo.
-	if exist('parameters.mat', 'file') ~= 2
-		display('Falha ao carregar arquivo de parâmetros');
-	else
+function load_parameters(print)
+%LOAD_PARAMETERS Carrega parâmetros do modelo na variável global parameters.
+%	É a única variável global mantida no projeto. Se print é usado como parâmtro,
+%	os mostra na tela.
+	global parameters;
+
+	if exist('parameters.mat', 'file') == 2
 		load('parameters.mat');
+	else
+		display('Parâmetros não encontrados, usando valores padrões.');
+		parameters = struct('k', 1, 'ro', 1, 'cp', 1, 'alpha', [], 'length', 0.01, 'subdiv', 10);
+	end
+	parameters.alpha = parameters.k / (parameters.ro * parameters.cp);
+
+	if nargin > 0
 		display('Parâmetros atuais:');
 		display(sprintf('k (condutividade térmica) = %e [W / (m * K)]', parameters.k));
 		display(sprintf('ro = ρ (massa específica) = %e [kg / (m³)]', parameters.ro));
